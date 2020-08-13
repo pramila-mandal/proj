82a9371eac46493ca3668ddd992b6536c9c47192
Pintu Mondal <pintu@Pintus-MacBook-Air.local>
2020-08-13 16:45:22 +0530

    changing sqlite to mysql

--- proj/settings.py
+++ proj/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'proj.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'ftl',
+        'USER': 'pramilamandal',
+        'PASSWORD': 'ntpc4477',
+        'HOST': 'pramilamandal.mysql.pythonanywhere-services.com',
+        'PORT': '3306',
     }
 }
 
